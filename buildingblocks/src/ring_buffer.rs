55e4d58f445917bf49672b2d8c4398a0647392ae
Ondřej Hošek <ondra.hosek@gmail.com>
2022-09-06 15:16:06 +0200

    rewrite ring buffer code (WIP)

--- buildingblocks/src/ring_buffer.rs
+++ buildingblocks/src/ring_buffer.rs
@@ -1,68 +1,83 @@
+use core::mem::MaybeUninit;
+
+
 /// A circular buffer of a preset capacity. If a value is appended to a full ring buffer, the oldest
 /// element is replaced with the newest one.
 #[derive(Clone, Copy, Debug)]
 pub struct RingBuffer<T, const SIZE: usize> {
-    buffer: [T; SIZE],
+    buffer: [MaybeUninit<T>; SIZE],
     first_set: usize,
-    last_set_plus_one: usize,
+    count_set: usize,
 }
 impl<T, const SIZE: usize> RingBuffer<T, SIZE> {
     /// Returns the capacity of this buffer, i.e. its length when it is entirely full.
     #[inline]
-    pub fn capacity(&self) -> usize { SIZE }
+    pub const fn capacity(&self) -> usize { SIZE }
 
     /// Returns the number of occupied elements within the buffer.
     #[inline]
-    pub fn len(&self) -> usize {
-        if self.first_set > self.last_set_plus_one {
-            // 0123456789
-            // ####   ###
-            // fs=7, lsp1=4, capa=10
-            //
-            // 0123456789
-            //      #####
-            // fs=5, lsp1=0, capa=10
-            (self.capacity() - self.first_set) + self.last_set_plus_one
+    pub const fn len(&self) -> usize { count_set }
+
+    /// Creates a new, empty ring buffer.
+    pub const fn new() -> Self {
+        Self {
+            buffer: [MaybeUninit::uninit(); SIZE],
+            first_set: 0,
+            count_set: 0,
+        }
+    }
+
+    /// Attempts to add a value to the end of the buffer.
+    ///
+    /// Returns whether the value was added successfully.
+    #[inline]
+    pub const fn push(&mut self, value: T) -> bool {
+        if self.count_set == SIZE {
+            false
         } else {
-            // 0123456789
-            //   #####
-            // fs=2, lsp1=7, capa=10
-            //
-            // 0123456789
-            // ####
-            // fs=0, lsp1=4, capa=10
-            //
-            // 0123456789
-            // ##########
-            // fs=0, lsp1=
-            self.last_set_plus_one - self.first_set
+            let index = (self.first_set + self.count_set) % SIZE;
+            self.buffer[index] = MaybeUninit::new(value);
+            self.count_set += 1;
+            true
+        }
+    }
+
+    /// Attempts to remove a value from the beginning of the buffer.
+    #[inline]
+    pub const fn pop(&mut self) -> Option<T> {
+        if self.count_set == 0 {
+            None
+        } else {
+            let val = core::mem::replace(
+                &mut self.buffer[self.first_set],
+                MaybeUninit::uninit(),
+            );
+            self.first_set = (self.first_set + 1) % SIZE;
+            self.count_set -= 1;
         }
     }
 }
 
-impl<T: Default + Sized, const SIZE: usize> RingBuffer<T, SIZE> {
-    /// Creates a new, empty ring buffer.
-    pub const fn new() -> Self {
-        Self {
-            buffer: Self::make_buffer(),
-            first_set: 0,
-            last_set_plus_one: 0,
-        }
-    }
+struct Iter<'a, T, SIZE> {
+    ring_buffer: &'a RingBuffer<T, SIZE>,
+    index: usize,
+}
+impl Iterator for Iter<'a, T, SIZE> {
+    type Item = &'a T;
 
-    /// Creates the inner buffer for this ring buffer.
-    const fn make_buffer() -> [T; SIZE] {
-        use core::mem::MaybeUninit;
-        let mut data: [MaybeUninit<T>; SIZE] = unsafe {
-            MaybeUninit::uninit().assume_init()
-        };
-        for elem in &mut data[..] {
-            unsafe {
-                core::ptr::write(elem.as_mut_ptr(), T::default());
-            }
-        }
-        unsafe {
-            core::mem::transmute(data)
-        }
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::RingBuffer;
+
+    #[test]
+    fn test_empty() {
+        let mut rb: RingBuffer<u8, 32> = RingBuffer::new();
+        todo!();
     }
 }
