1b723578b25c828a3a7a2c19b6fc8f880cbf6cbb
Ondřej Hošek <ondra.hosek@gmail.com>
2022-09-22 02:06:23 +0200

    build.ps1: debugger support

--- on_board/build.ps1
+++ on_board/build.ps1
@@ -2,7 +2,11 @@
 Param (
     [Parameter(Position=0)]
     [string]
-    $ComPort
+    $ComPort,
+
+    [Parameter(Position=1)]
+    [string]
+    $DebugPort
 )
 
 
@@ -24,4 +28,13 @@ Write-Output ("{0:#,##0.###}" -f $kilobytes)
 If ($ComPort -ne "")
 {
     & 'C:\Program Files (x86)\BOSSA\bossac.exe' --arduino-erase --erase --write --boot=1 --port=$ComPort .\aehc
+}
+
+If ($DebugPort -ne "")
+{
+    & 'C:\Program Files\arm-gcc\bin\arm-none-eabi-gdb.exe' `
+        -ex "target extended-remote \\.\$DebugPort" `
+        -ex "monitor jtag_scan" `
+        -ex "attach 1" `
+        .\target\thumbv7m-none-eabi\release\arduino_enocean_heater_control
 }
