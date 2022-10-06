721e9874575a4622f9543a97b8e5422171f3661b
Ondřej Hošek <ondra.hosek@gmail.com>
2022-10-07 00:34:09 +0200

    build.ps1: allow overriding binary name

--- on_board/build.ps1
+++ on_board/build.ps1
@@ -9,7 +9,10 @@ Param (
     $DebugPort,
 
     [switch]
-    $NoBuild
+    $NoBuild,
+
+    [string]
+    $BinaryName="arduino_enocean_heater_control"
 )
 
 
@@ -21,19 +24,19 @@ If (-not $NoBuild)
         Return 1
     }
 
-    & rust-objcopy --output-target=binary .\target\thumbv7m-none-eabi\release\arduino_enocean_heater_control .\aehc
+    & rust-objcopy --output-target=binary ".\target\thumbv7m-none-eabi\release\$BinaryName" ".\$BinaryName.bin"
     If ($LASTEXITCODE -ne 0)
     {
         Return 1
     }
 }
 
-$kilobytes = (Get-Item -LiteralPath .\aehc).Length / 1024
-Write-Output ("{0:#,##0.###}" -f $kilobytes)
+$kilobytes = (Get-Item -LiteralPath ".\$BinaryName.bin").Length / 1024
+Write-Output ("{0:#,##0.###} KiB" -f $kilobytes)
 
 If ($ComPort -ne "")
 {
-    & 'C:\Program Files (x86)\BOSSA\bossac.exe' --arduino-erase --erase --write --boot=1 --port=$ComPort .\aehc
+    & 'C:\Program Files (x86)\BOSSA\bossac.exe' --arduino-erase --erase --write --boot=1 --port=$ComPort ".\$BinaryName.bin"
 }
 
 If ($DebugPort -ne "")
@@ -42,5 +45,5 @@ If ($DebugPort -ne "")
         -ex "target extended-remote \\.\$DebugPort" `
         -ex "monitor jtag_scan" `
         -ex "attach 1" `
-        .\target\thumbv7m-none-eabi\release\arduino_enocean_heater_control
+        ".\target\thumbv7m-none-eabi\release\$BinaryName"
 }
