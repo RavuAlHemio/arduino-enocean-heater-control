dc75c2b2ecbf7490956b6c06898613f37ee59eaf
Ondřej Hošek <ondra.hosek@gmail.com>
2022-11-24 00:48:01 +0100

    build.ps1: OpenOCD support
    Allow programming the Arduino using OpenOCD (JTAG/SWD) instead of the
    programming port.
    
    This allows us to keep listening to programming port traffic without
    having to keep handing over the port between serial terminal and BOSSA.

--- on_board/build.ps1
+++ on_board/build.ps1
@@ -12,19 +12,39 @@ Param (
     $NoBuild,
 
     [string]
-    $BinaryName="arduino_enocean_heater_control"
+    $BinaryName="arduino_enocean_heater_control",
+
+    [switch]
+    $ProgramOOCD,
+
+    [switch]
+    $DebugBuild,
+
+    [switch]
+    $DebugOOCD
 )
+
+
+$targetSubdir = If ($DebugBuild) { "debug" } Else { "release" }
 
 
 If (-not $NoBuild)
 {
-    & cargo build --release
+    If ($DebugBuild)
+    {
+        & cargo build
+    }
+    Else
+    {
+        & cargo build --release
+    }
+
     If ($LASTEXITCODE -ne 0)
     {
         Return 1
     }
 
-    & rust-objcopy --output-target=binary ".\target\thumbv7m-none-eabi\release\$BinaryName" ".\$BinaryName.bin"
+    & rust-objcopy --output-target=binary ".\target\thumbv7m-none-eabi\$targetSubdir\$BinaryName" ".\$BinaryName.bin"
     If ($LASTEXITCODE -ne 0)
     {
         Return 1
@@ -34,18 +54,40 @@ If (-not $NoBuild)
 $kilobytes = (Get-Item -LiteralPath ".\$BinaryName.bin").Length / 1024
 Write-Output ("{0:#,##0.###} KiB" -f $kilobytes)
 
-If ($ComPort -ne "")
+If ($ProgramOOCD)
+{
+    & "C:\Program Files\OpenOCD\bin\openocd.exe" `
+        -c "set BINFILE $BinaryName.bin" `
+        -c "source oocd-prog-jlink.cfg"
+}
+ElseIf ($ComPort -ne "")
 {
     # disable DTR on the COM port before running BOSSA
     & '..\on_host\target\debug\winsersetup.exe' --disable-dtr $ComPort
     & 'C:\Program Files (x86)\BOSSA\bossac.exe' --arduino-erase --erase --write --boot=1 --port=$ComPort ".\$BinaryName.bin"
 }
 
-If ($DebugPort -ne "")
+If ($DebugOOCD)
+{
+    $oocd = Start-Process `
+        -FilePath "pwsh.exe" `
+        -ArgumentList @("-NoExit -Command & \`"C:\Program Files\OpenOCD\bin\openocd.exe\`" -c \`"source oocd-debug-jlink.cfg\`"") `
+        -PassThru
+
+    $gdb = Start-Process `
+        -FilePath 'C:\Program Files\arm-gcc\bin\arm-none-eabi-gdb.exe' `
+        -ArgumentList @("`"-ex`" `"target extended-remote :3333`" `".\target\thumbv7m-none-eabi\$targetSubdir\$BinaryName`"") `
+        -PassThru
+
+    Write-Output "Exit GDB and OpenOCD to return to console."
+    $oocd.WaitForExit()
+    $gdb.WaitForExit()
+}
+ElseIf ($DebugPort -ne "")
 {
     & 'C:\Program Files\arm-gcc\bin\arm-none-eabi-gdb.exe' `
         -ex "target extended-remote \\.\$DebugPort" `
         -ex "monitor jtag_scan" `
         -ex "attach 1" `
-        ".\target\thumbv7m-none-eabi\release\$BinaryName"
+        ".\target\thumbv7m-none-eabi\$targetSubdir\$BinaryName"
 }
